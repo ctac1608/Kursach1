4d78d26f843bc12c0396bccdd7aa3ba5bb633d1b
ctac1608 <sheludchenko.stas@gmail.com>
2016-11-05 16:45:51 +0300

    Add project files.

---
+++ Kursach2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Kursach2.Startup))]
+namespace Kursach2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
